31ff2c82adc93a877f349789cc2d3c508c5c6481
Phoscur <phoscur@pheelgood.net>
2014-11-17 01:00:05 +0100

    use $_REQUEST for QR and NFC submit

--- multi/login.php
+++ multi/login.php
@@ -15,18 +15,18 @@ if (!$_REQUEST['log']) {
 	global $redirurl;
 	$clientip = $_SERVER['REMOTE_ADDR'];
 	$clientmac = arp_get_mac_by_ip($clientip);
-	$tempurl = explode("redirurl=", $_GET['redirurl']);
+	$tempurl = explode("redirurl=", $_REQUEST['redirurl']);
 	if ($tempurl[1] != "" && $tempurl[1] != "/index.php") {
 		$redirurl = $tempurl[1];
 	} else {
 		$redirurl = $defaultRedirectUrl;
 	}
-	if (local_backed($_GET['usr'], $_GET['pwd'])) {
+	if (local_backed($_REQUEST['usr'], $_REQUEST['pwd'])) {
 		logging("access granted");
 		portal_allow($clientip, $clientmac, "nfc");
 	} else {
 		logging("access denied");
 		include("captiveportal-login.html");
-		echo("<center>Login rejected. access denied</center>");
+		echo('<div class="error">Login rejected - access denied</div>');
 	}
 }
